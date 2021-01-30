b1e379ec90dab0946cd3143530b8d002e8eb0fcb
climb719 <mary.oldweiler@gmail.com>
2021-01-30 11:27:51 -0500

    commit tree f1971e134f7b7f08b338ff8b462ae9cf3cc7167f

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,12 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_reader :brand #only need a reader since ititialize method has logic for creating the class constant
+
+  BRANDS = [] #defining BRANDS as an empty array sinse plural and need to store multiple
 
   def initialize(brand)
-    @brand = brand
+    @brand = brand #keeps track of all brands
+    BRANDS << brand unless BRANDS.include?(brand) # only keeps track of unique brands
   end
 
   def cobble
